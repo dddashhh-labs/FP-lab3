dbce1f628d9388a57ff0c53a97ef7ef980cd06fa
Daria Karaseva <76691263+dddashhh@users.noreply.github.com>
2025-12-13 02:23:23 +0300

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,8 +1,16 @@
 import Lake
 open Lake DSL
 
-package interpolation
+package «interpolation» where
+  -- add package configuration options here
+
+lean_lib «Interpolation» where
+  -- add library configuration options here
 
 @[default_target]
-lean_exe interpolation where
+lean_exe «interpolation» where
   root := `Main
+  -- Enables the use of the Lean interpreter by the executable (e.g.,
+  -- `runFrontend`) at the expense of increased binary size on Linux.
+  -- Remove this line if you do not need such functionality.
+  supportInterpreter := true
