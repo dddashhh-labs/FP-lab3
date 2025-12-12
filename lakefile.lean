6f7631a136edccfb4f68a0c31dbeab68291ca452
Daria Karaseva <76691263+dddashhh@users.noreply.github.com>
2025-12-13 02:15:14 +0300

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -5,21 +5,10 @@ package «interpolation» where
   -- add package configuration options here
 
 lean_lib «Interpolation» where
-  -- add library configuration options here
-  roots := #[
-    `Interpolation.Types,
-    `Interpolation.Parser,
-    `Interpolation.Linear,
-    `Interpolation.Newton,
-    `Interpolation.PointGenerator,
-    `Interpolation.SlidingWindow,
-    `Interpolation.StreamProcessor,
-    `Interpolation.Input,
-    `Interpolation.Output,
-    `Interpolation.Stream,
-    `Interpolation.CLI
-  ]
+  -- Явный порядок компиляции модулей
+  globs := #[.andSubmodules `Interpolation]
 
 @[default_target]
 lean_exe «interpolation» where
   root := `Main
+  supportInterpreter := true
